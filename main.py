487edbb3fbd9bade61fcc21373a4fc429ace7bb3
hahaitsrose <33584988+hahaitsrose@users.noreply.github.com>
2025-01-30 23:13:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import functions.albumfuncs
 import functions.trackfuncs
 
 data_link = input("Enter the link you wish to check (track or album)\n")
-album_or_single = input("Album or Track?\n")
-album_or_single_lower = album_or_single.lower()
 
 to_print = ""
 
-if album_or_single_lower == "track":
+if "/track/" in data_link:
     to_print = functions.trackfuncs.get_stream_count(data_link)
-elif album_or_single_lower == "album":
+if "/album/" in data_link:
     to_print = functions.albumfuncs.get_stream_count(data_link)
 else:
-    print(f"{album_or_single_lower} is not a valid type to check for.")
+    print(f"{data_link} is not a valid type to check for.")
 
 print(to_print)
 
