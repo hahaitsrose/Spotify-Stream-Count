736e3ce08a3349989f32d88ab8179a6b09988ef7
hahaitsrose <33584988+hahaitsrose@users.noreply.github.com>
2025-01-30 23:15:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ to_print = ""
 
 if "/track/" in data_link:
     to_print = functions.trackfuncs.get_stream_count(data_link)
-if "/album/" in data_link:
+elif "/album/" in data_link:
     to_print = functions.albumfuncs.get_stream_count(data_link)
 else:
     print(f"{data_link} is not a valid type to check for.")
